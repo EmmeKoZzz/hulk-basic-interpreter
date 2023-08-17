91b03d59506dee3bddc75e54a4312c02f85fa954
EmmeKoZzz <emme.ko.zzz@gmail.com>
2023-08-17 08:30:08 -0400

    create greetings

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace HULK_Interpreter;
+
+class Program {
+	public static void Main() {
+		Welcome();
+		while (true) {
+			if (Ask(out string input)) continue;
+			Console.WriteLine(input);
+			return;
+		}
+	}
+
+	static void Welcome() =>
+		Console.WriteLine(
+			"HI, this is a Simplify HULK Interpreter (SHULKI):\n " +
+			"Write the expression to be interpreted or use the command 'run <filepath>' to read it from file."
+		);
+
+
+	static bool Ask(out string answer) {
+		Console.Write("> ");
+		answer = Console.ReadLine() ?? "";
+		return String.IsNullOrEmpty(answer);
+	}
+}
